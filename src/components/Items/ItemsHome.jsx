5aae9c5b3d1362b32222d01b223c455b882dfd59
Pieter Verschragen <pieter@qlouder.com>
2016-11-11 20:50:24 +0100

    prototype json table with data from sheets.

--- src/components/Items/ItemsHome.jsx
+++ src/components/Items/ItemsHome.jsx
@@ -2,32 +2,103 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import 'whatwg-fetch'
+import { withRouter } from 'react-router';
 
 class ItemsHome extends Component {
   constructor(props) {
     super(props);
     window.sourceSheet = '1HktYmh9zKprS5YrBWMakZKIfKNIa7bV9X7fp_bpaAfM';
-    loadSheetList();
+    this.updateData = this.updateData.bind(this);
+    this.loadData = this.loadData.bind(this);
+    loadSheetList(this.loadData);
   }
+
   render() {
+    if (this.state != null && this.state.data != null) {
+     console.log("Rendering data.");
     return (
-      <div>
-        <h4>ItemsHome Container</h4>
-      </div>
+        <div>
+        <GenericJsonTable data={this.state.data}/>
+        </div>
+      );
+    } else {
+      console.log("Rendering loading.");
+      return (<div>loading...</div>);
+    }
+  }
+
+  loadData(updateData) {
+    var sheetMap = initSheetList(updateData,this.loadItems)
+    this.setState({sheetList:sheetMap});
+    loadItemList(sheetMap.Swords, this.updateData);
+  }
+  updateData(data) {
+    var rows = initItemList(data);
+    this.setState({data:rows});
+  }
+}
+
+class GenericJsonTable extends Component {
+  render() {
+      var rows = []
+      var header = <GenericJsonTableHeaderRow data={this.props.data[0]}/>
+      rows.push(header);
+
+      this.props.data.forEach(function(item, index) {
+        rows.push(<GenericJsonTableRow data = {item}/>);
+        console.log(rows.length);
+      });
+
+      return ( <div> {rows} </div>);
+  }
+
+}
+class GenericJsonTableHeaderRow extends Component {
+  render () {
+    var cells = [];
+    var keys = Object.keys(this.props.data);
+    keys.forEach(function(key,index,cells)  {
+       cells.push(<GenericJsonTableHeaderCell value={key}/>);
+    });
+    return (
+      <div>{cells}</div>
     )
   }
 }
 
+class GenericJsonTableRow extends Component {
+  render () {
+    var cells = [];
+    for (var property in this.props.data) {
+      if (this.props.data.hasOwnProperty(property)) {
+        cells.push(<GenericJsonTableCell value={property}/>);
+      }
+    } 
+    return (
+      <div>{cells}</div>
+    )
+  }
+}
 
-function loadSheetList() {
+class GenericJsonTableCell extends Component {
+  render() {
+    return (<div>{this.props.value}</div>);
+  }
+}
+
+class GenericJsonTableHeaderCell extends Component {
+  render() {
+    return (<div><h1>{this.props.value}</h1></div>);
+  }
+}
+
+
+function loadSheetList(callback) {
   fetch('https://spreadsheets.google.com/feeds/worksheets/'+window.sourceSheet+'/public/basic?alt=json')
   .then(function(response) {
       return response.text();
-    }).then(function(sheetsJson) {
-      initSheetList(sheetsJson)
-    })
+    }).then(callback);
 }
-
 function initSheetList(sheetsJson) {
   var sheets = JSON.parse(sheetsJson);
   var entries = sheets.feed.entry;
@@ -41,23 +112,17 @@ function initSheetList(sheetsJson) {
       id = id.substring(lastSlash+1,id.length);
       sheetMap[title] = id;
     }
-    window.sheetMap = sheetMap;
-
-    loadItemList();
+    return sheetMap;
 }
 
-
-
-function loadItemList() {
-    var swordsId = window.sheetMap.Swords;
-    fetch('https://spreadsheets.google.com/feeds/list/'+window.sourceSheet+'/'+swordsId+'/public/basic?alt=json')
+function loadItemList(sheetId, callback) {
+    fetch('https://spreadsheets.google.com/feeds/list/'+window.sourceSheet+'/'+sheetId+'/public/basic?alt=json')
     .then(function(response) {
       return response.text();
-    }).then(function(sheetsJson) {
-      initItemList(sheetsJson)
-    })
+    }).then(callback)
 
 }
+
 function initItemList(sheetsJson) {
   var sheet = JSON.parse(sheetsJson);
   var rows = sheet.feed.entry;
@@ -73,7 +138,8 @@ function initItemList(sheetsJson) {
     }
     parsedRows[i] = row;
   }
-  console.log(JSON.stringify(rows));
+  console.log(parsedRows.length);
+  return parsedRows;
 }
 
 
