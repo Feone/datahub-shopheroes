333aacc21a61379968011e8925c9f026ae6102ef
Pieter Verschragen <pieter@qlouder.com>
2016-11-12 15:57:46 +0100

    Attempt at data loading instead of static config.

--- src/components/DataHub.jsx
+++ src/components/DataHub.jsx
@@ -9,23 +9,17 @@ import 'whatwg-fetch';
 export default class DataHub extends Component {
   constructor() {
     super();
-    this.worksheets = 'https://spreadsheets.google.com/feeds/worksheets';
-    this.listsheets = 'https://spreadsheets.google.com/feeds/list';
-    this.sourceSheet = '1HktYmh9zKprS5YrBWMakZKIfKNIa7bV9X7fp_bpaAfM';
+    this.worksheets = 'https://spreadsheets.google.com/feeds/worksheets/'; //get list of sheets url
+    this.listsheets = 'https://spreadsheets.google.com/feeds/list/'; //get sheet data url
+    this.sourceSheet = '1HktYmh9zKprS5YrBWMakZKIfKNIa7bV9X7fp_bpaAfM'; //Id of the backend spreadsheet
     this.state = {
-
     }
-
-    // this.initSheetList = this.initSheetList.bind(this);
-    // this.updateData = this.updateData.bind(this);
-    // this.loadData = this.loadData.bind(this);
-    // this.loadSheetList = this.loadSheetList.bind(this);
-    // this.loadItemList = this.loadItemList.bind(this);
+    this.loadSheetList = this.loadSheetList.bind(this); //Bind so items can be used in a callback.
+    this.setSheetList = this.setSheetList.bind(this);
+    this.loadDataFromSheet = this.loadDataFromSheet.bind(this);
+    this.initDataFromSheet = this.initDataFromSheet.bind(this);
+    this.loadData = this.loadData.bind(this);
   }
-
-
-
-
 
   /////////////////////////////////////////////////////////////////////
   /**
@@ -39,57 +33,86 @@ export default class DataHub extends Component {
   /////////////////////////////////////////////////////////////////////
 
 
-
-
-
-
-
   // call initialization functions here
   componentDidMount() {
     console.log('Initializing, should only be called once to fetch data for the whole SPA');
     
-    // this.loadSheetList(this.loadData);
+     this.loadSheetList(this.setSheetList);
+  }
+  //Uses an http-get to retrieve a list of sheets on the source spreadsheet, specifies the setSheetList as the callback to deal with the data.
+  loadSheetList(callback) {
+    fetch(this.worksheets + this.sourceSheet + '/public/basic?alt=json')
+      .then(function (response) {
+        return response.text();
+      }).then(callback);
   }
 
-  // loadData(updateData) {
-  //   const sheetMap = initSheetList(updateData, this.loadItems)
-  //   this.setState({ sheetList: sheetMap });
-  //   loadItemList(sheetMap.Swords, this.updateData);
-  // }
 
-  // updateData(data) {
-  //   const rows = initItemList(data);
-  //   this.setState({ data: rows });
-  // }
+  //Parses the raw sheet list json data and places it in the props as an object of sheetName:sheetKey parameters.
+  setSheetList(sheetsJson) {
+    console.log("Placing the list of spreadsheets on the props.");
+    const sheets = JSON.parse(sheetsJson);
+    const entries = sheets.feed.entry;
+    const arrayLength = entries.length;
+    const sheetMap = {};
+    for (var i = 0; i < arrayLength; i++) {
+      const title = entries[i].title.$t;
+      var id = entries[i].id.$t;
+      const lastSlash = id.lastIndexOf('/');
+      id = id.substring(lastSlash + 1, id.length);
+      sheetMap[title] = id;
+    }
+    this.setState({ sheetList: sheetMap });
+  }
 
-  // loadSheetList(callback) {
-  //   fetch(this.spreadsheet + this.sourceSheet + '/public/basic?alt=json')
-  //     .then(function (response) {
-  //       return response.text();
-  //     }).then(callback);
-  // }
+  //Function that reads the data of a specific sheet from the source, by name. 
+  //To be called by any component looking to load data, returns the data if it's loaded, loads it if not.
+  loadData(sheetName) {
+    console.log("Loading data for: "+sheetName);
+    if (props.sheetList == null) {
+      console.log("Load sheet called before sheetlist was initialized, cancelling load request.");
+      return null;
+    }
+    if (props.datasheets[sheetName] != null) {
+      return props.sheets[sheetName];
+    } else {
+      loadDataFromSheet(sheetName,this.initDataFromSheet);
+    }
+  }
+  //Function that uses an http get request to get the contents of the specified sheet, grabbing the key from the props mapping. 
+  //Should only be called if setSHeetList has already completed.
+  loadDataFromSheet(sheetName, callback) {
+    const sheetId = this.props.sheetList[sheetName];
+    fetch(this.listsheets + this.sourceSheet + '/' + sheetId + '/public/basic?alt=json')
+      .then(function (response) {
+        return response.text();
+      }).then(callback)
+  }
 
-  // initSheetList(sheetsJson) {
-  //   const sheets = JSON.parse(sheetsJson);
-  //   const entries = sheets.feed.entry;
-  //   const arrayLength = entries.length;
-  //   const sheetMap = {};
-  //   for (let i = 0; i < arrayLength; i++) {
-  //     const title = entries[i].title.$t;
-  //     const id = entries[i].id.$t;
-  //     let lastSlash = id.lastIndexOf('/');
-  //     id = id.substring(lastSlash + 1, id.length);
-  //     sheetMap[title] = id;
-  //   }
-  //   return sheetMap;
-  // }
+  //Adds received data from a spreadsheet to the props.
+  initDataFromSheet(sheetsJson) {
+    const sheet = JSON.parse(sheetsJson);
+    const rows = sheet.feed.entry;
+    var parsedRows = [];
+    const title = sheet.feed.title.$t;
+    for (var i = 0; i < rows.length; i++) {
+      var row = {}
+      row.itemName = rows[i].title.$t;
+      const otherCells = rows[i].content.$t;
+      const cells = otherCells.split(',');
+      for (var j = 0; j < cells.length; j++) {
+        const pieces = cells[j].split(":");
+        row[pieces[0]] = pieces[1];
+      }
+      parsedRows[i] = row;
+    }
+    console.log("Placing loaded data on props.datasheets."+title);
+    const propsData = {}; //add it to the props under datasheets/<sheetname>
+    propsData[title]=sheetMap;
+    this.setState({datasheets:propsData});
+  }
 
-  // loadItemList(sheetId, callback) {
-  //   fetch(this.spreadsheet + this.sourceSheet + '/' + sheetId + '/public/basic?alt=json')
-  //     .then(function (response) {
-  //       return response.text();
-  //     }).then(callback)
-  // }
+
 
   render() {
     // clone children components, while adding props to them
@@ -103,6 +126,9 @@ export default class DataHub extends Component {
          * swords it will trigger to load the swords item list so that the view
          * can display them
          */ 
+        loadData: this.loadData, //Function any component can use to get and/or load data. 
+        sheetDdata: this.sheetData, //Container of all sheet data already loaded.
+        sheetList: this.sheetList //List of spreadsheets on the source, in case that's relevant somewhere. Should also trigger an update on any children so they can re-attempt to load data I  think, not sure. 
 
         // loadItemList: this.loadItemList
       });
--- src/components/Items/CategoryView.jsx
+++ src/components/Items/CategoryView.jsx
@@ -4,45 +4,48 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import {
-  sword, dagger, axe, spear, mace, staff, bow, gun,
-  armor, vest, helmet, gauntlet, boots, clothes,
-  shield, hat, bracer, footwear, remedy, potion,
-  spell, projectile, ring, pendant, instruments
-} from './itemNames';
+// import {
+//   sword, dagger, axe, spear, mace, staff, bow, gun,
+//   armor, vest, helmet, gauntlet, boots, clothes,
+//   shield, hat, bracer, footwear, remedy, potion,
+//   spell, projectile, ring, pendant, instruments
+// } from './itemNames';
 
 export default ({ params: {category} }) => {
   const links = [];
-
-  switch (category) {
-    case 'sword': createLinks(category, sword); break;
-    case 'dagger': createLinks(category, dagger); break;
-    case 'axe': createLinks(category, axe); break;
-    case 'spear': createLinks(category, spear); break;
-    case 'mace': createLinks(category, mace); break;
-    case 'staff': createLinks(category, staff); break;
-    case 'bow': createLinks(category, bow); break;
-    case 'gun': createLinks(category, gun); break;
-    case 'armor': createLinks(category, armor); break;
-    case 'vest': createLinks(category, vest); break;
-    case 'helmet': createLinks(category, helmet); break;
-    case 'gauntlet': createLinks(category, gauntlet); break;
-    case 'boots': createLinks(category, boots); break;
-    case 'clothes': createLinks(category, clothes); break;
-    case 'shield': createLinks(category, shield); break;
-    case 'hat': createLinks(category, hat); break;
-    case 'bracer': createLinks(category, bracer); break;
-    case 'footwear': createLinks(category, footwear); break;
-    case 'remedy': createLinks(category, remedy); break;
-    case 'potion': createLinks(category, potion); break;
-    case 'spell': createLinks(category, spell); break;
-    case 'projectile': createLinks(category, projectile); break;
-    case 'ring': createLinks(category, ring); break;
-    case 'pendant': createLinks(category, pendant); break;
-    case 'instruments': createLinks(category, instruments); break;
-  }
+  createLinks(category, this.props.loadData(category));
+  // switch (category) {
+  //   case 'sword': createLinks(category, sword); break;
+  //   case 'dagger': createLinks(category, dagger); break;
+  //   case 'axe': createLinks(category, axe); break;
+  //   case 'spear': createLinks(category, spear); break;
+  //   case 'mace': createLinks(category, mace); break;
+  //   case 'staff': createLinks(category, staff); break;
+  //   case 'bow': createLinks(category, bow); break;
+  //   case 'gun': createLinks(category, gun); break;
+  //   case 'armor': createLinks(category, armor); break;
+  //   case 'vest': createLinks(category, vest); break;
+  //   case 'helmet': createLinks(category, helmet); break;
+  //   case 'gauntlet': createLinks(category, gauntlet); break;
+  //   case 'boots': createLinks(category, boots); break;
+  //   case 'clothes': createLinks(category, clothes); break;
+  //   case 'shield': createLinks(category, shield); break;
+  //   case 'hat': createLinks(category, hat); break;
+  //   case 'bracer': createLinks(category, bracer); break;
+  //   case 'footwear': createLinks(category, footwear); break;
+  //   case 'remedy': createLinks(category, remedy); break;
+  //   case 'potion': createLinks(category, potion); break;
+  //   case 'spell': createLinks(category, spell); break;
+  //   case 'projectile': createLinks(category, projectile); break;
+  //   case 'ring': createLinks(category, ring); break;
+  //   case 'pendant': createLinks(category, pendant); break;
+  //   case 'instruments': createLinks(category, instruments); break;
+  // }
 
   function createLinks(itemType, itemArray) {
+    if (itemArray == null) {
+      return (<div className = "category-view">Loading data...</div>);
+    }
     itemArray.forEach(item => {
       const itemName = item.toLowerCase().replace(/ /, '');
       const to = `/items/${itemType}/${itemName}`;
